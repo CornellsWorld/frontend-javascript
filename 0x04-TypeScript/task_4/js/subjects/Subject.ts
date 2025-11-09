fb62849cd70b30c714d498348b0206870f26db4f
CornellsWorld <corneliusalhassan@gmail.com>
2025-11-09 14:46:46 +0100

    up

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,12 +1,12 @@
 /// <reference path="./Teacher.ts" />
 
 namespace Subjects {
-  export interface Subject {
+  interface Subject {
     teacher: Teacher;
     setTeacher(teacher: Teacher): void;
   }
 
-  export class Subject implements Subject {
+  class Subject implements Subject {
     teacher: Teacher;
 
     setTeacher(teacher: Teacher): void {
